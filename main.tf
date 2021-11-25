6fa53c261aa6a94d13d23e5a64ff15d13df8f83c
Jeff Brown Tech <jeff@jeffbrown.tech>
2021-11-25 09:23:27 -0800

    fixed sa module dependency

--- main.tf
+++ main.tf
@@ -19,8 +19,8 @@ module "sa" {
   source = "./modules/storage_account"
 
   storage_account_name = var.storage_account_name
-  resource_group_name  = var.resource_group_name
-  location             = var.location
+  resource_group_name  = azurerm_resource_group.rg.name
+  location             = azurerm_resource_group.rg.location
 }
 
 module "container" {
